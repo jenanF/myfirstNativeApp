9e58409c29ca0a63e08a67b9af17599d8e231005
jenanF <108689302+jenanF@users.noreply.github.com>
2024-12-23 15:03:29 +0300

    first commit

---
+++ Components/Categories.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const Categories = () => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+export default Categories
